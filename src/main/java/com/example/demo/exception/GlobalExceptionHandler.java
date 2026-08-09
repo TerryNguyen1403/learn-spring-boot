a54202b961eb8e39b1f0d11614b948435c159c97
TerryNguyen1403 <giangnht1403@gmail.com>
2026-08-09 19:41:20 +0700

    feat: create ResourceNotFoundException

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.demo.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<String> methodArgumentNotValidExceptionHandler(ResourceNotFoundException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
+	}
+}
