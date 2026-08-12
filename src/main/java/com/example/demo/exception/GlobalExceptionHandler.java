5e8ff4fd78c8472b5f23a3034023b9126d5a2d10
TerryNguyen1403 <giangnht1403@gmail.com>
2026-08-12 17:59:14 +0700

    modify: modify GlobalException return

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,13 +1,42 @@
 package com.example.demo.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<String> methodArgumentNotValidExceptionHandler(ResourceNotFoundException e) {
-		return ResponseEntity.badRequest().body(e.getMessage());
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<ErrorResponse> userNotFoundExceptionHandler(UserNotFoundException e) {
+		ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationErrorResponse> methodArgumentNotValidExceptionHandler(
+			MethodArgumentNotValidException e) {
+		Map<String, String> errors = new HashMap<>();
+		e.getBindingResult().getFieldErrors().forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
+
+		ValidationErrorResponse body = new ValidationErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid data",
+				errors);
+
+		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
+		ErrorResponse body = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal error");
+
+		log.error("Unhandle {}", e);
+		return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
